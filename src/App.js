3a70c1c6611348dd7be314ecf4038c81b018114f
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-07 10:14:16 +0100

    React-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import Counter from './Counter'
 import PassingProps from './PassingProps'
 
 
 const App = () => (
-  <div>
-
-    <Counter
-    />
-    <PassingProps
-    />
-  </div>
+  <Router>
+    <div>
+      <Route exact path="/" component={Counter} />
+      <Route exact path="/counter" component={Counter} />
+      <Route exact path="/passing-props" component={PassingProps} />
+    </div>
+  </Router>
 )
 
 export default App;
